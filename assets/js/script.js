20eb9ac6622868e8118ac26ecaca4db263bb213a
Robert Tilton <robtilton2086@gmail.com>
2021-02-16 13:42:41 -0500

    initial html/javascript edits to add reddit api implementation into the site

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
-const fetch = require("node-fetch");
-
-fetch('https://coinmap.org/api/v1/venues/?query=bitcoin', {})
+fetch('https://www.reddit.com/r/escapefromtarkov.json', {
+})
 	.then(function(response) {
 		return response.json();
 	})
 	.then(
 		function(data) {
 			console.log(data);
+			var redditData = document.getElementById('reddit-bar');
+			redditData.textContent = JSON.stringify(data);
 		}
 	)
 ;
