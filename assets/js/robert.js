a40ec1e9d98ab4de32ff98b3bfbc3066f233084c
Robert Tilton <robtilton2086@gmail.com>
2021-02-16 15:18:31 -0500

    built out a for loop that may or may not work, but the beginnings of the loop and the reddit function are taking shape

--- assets/js/robert.js
+++ assets/js/robert.js
@@ -1,13 +1,17 @@
-fetch('https://www.reddit.com/r/escapefromtarkov.json', {
+fetch('https://www.reddit.com/r/wallstreetbets.json', {
 })
 	.then(function(response) {
 		return response.json();
 	})
 	.then(
 		function(data) {
-			console.log(data);
 			var redditData = document.getElementById('reddit-bar');
-			redditData.textContent = JSON.stringify(data);
+			console.log(data.data.children[0]);
+			var i;
+			for (i = 0; i <= 5; i++) {
+				redditData.textContent = JSON.stringify(data.data.children[i]);
+			}
+			// data.after.children[12]
 		}
 	)
 ;
