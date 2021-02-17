aae22d9ce1f46c09173159ee28ff6e87ab500405
Robert Tilton <robtilton2086@gmail.com>
2021-02-17 13:17:59 -0500

    added comments to my code

--- assets/js/robert.js
+++ assets/js/robert.js
@@ -1,24 +1,31 @@
+// declared variable linking to the Reddit div in the HTML
 var redditContent = document.getElementById('reddit-content');
-
+// fetching the data from reddit
 fetch('https://www.reddit.com/r/wallstreetbets/hot.json', {
 })
-	.then(function(response) {
+	.then(function (response) {
 		return response.json();
 	})
+	// function that parses the returned array and applies it where necessary
 	.then(
-		function(data) {
+		function (data) {
 			console.log(redditContent);
 			console.log(data.data.children[0]);
 			for (var i = 0; i < data.data.children.length; i++) {
 				if ((data.data.children[i].data.score) > 500) {
+					// post titles parsing
 					var redditPosts = document.createElement('h3');
 					redditPosts.textContent = data.data.children[i].data.title;
+					// username parsing
 					var redditUsers = document.createElement('h3');
-					redditUsers.textContent= data.data.children[i].data.author;
+					redditUsers.textContent = data.data.children[i].data.author;
+					// upvote/score parsing
 					var redditScore = document.createElement('h3');
-					redditScore.textContent= data.data.children[i].data.score;
+					redditScore.textContent = data.data.children[i].data.score;
+					// url parsing, at the moment not utilized
 					var redditURL = document.createElement('h3');
-					redditURL.textContent= data.data.children[i].data.url;
+					redditURL.textContent = data.data.children[i].data.url;
+					// adding the results to the page
 					redditContent.appendChild(redditPosts);
 					redditContent.appendChild(redditUsers);
 					redditContent.appendChild(redditScore);
@@ -26,4 +33,4 @@ fetch('https://www.reddit.com/r/wallstreetbets/hot.json', {
 			}
 		}
 	)
-;
+	;
