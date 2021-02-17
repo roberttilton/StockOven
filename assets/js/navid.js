4e404858b439a0db7a470669cecc56cf4e2843fc
Navid Ebahimi <navidmebrahimi22@gmail.com>
2021-02-17 13:50:51 -0500

    going a different approach

--- assets/js/navid.js
+++ assets/js/navid.js
@@ -29,62 +29,19 @@ fetch("https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-charts?symbol=
 	console.log(data);
 });
 
-function drawChart() {
-	anychart.data.loadJsonFile("https://cdn.anychart.com/samples/stock-general-features/load-json-data/data.json", function (data) {
-        // create stock chart
-        var chart = anychart.stock();
-
-        // create data table on loaded data
-        var dataTable = anychart.data.table();
-        dataTable.addData(data);
-
-        // map loaded data
-        var mapping = dataTable.mapAs({'low': 1, 'high': 2});
-
-        // create first plot on the chart with column series
-        var plot = chart.plot(0);
-
-        var series = plot.rangeColumn(mapping).name('Temperature');
-        series.tooltip()
-                .format(function () {
-                    return this.seriesName +
-                            '<br/><span style="color: #ccc">Max</span>: ' + this.high + '&deg;' +
-                            '<br/><span style="color: #ccc">Min</span>: ' + this.low + '&deg;';
-                });
-
-        // set grid settings
-        plot.yGrid(true)
-                .xGrid(true)
-                .yMinorGrid(true)
-                .xMinorGrid(true);
-
-        // create scroller series with mapped data
-        chart.scroller().rangeColumn(mapping);
-
-        // set tooltip settings
-        chart.tooltip().useHtml(true);
-
-        // set chart selected date/time range
-        chart.selectRange('2007-01-07', '2008-01-06');
-
-        // set container id for the chart
-        chart.container('yahoo-top');
-        // initiate chart drawing
-        chart.draw();
-
-        // create range picker
-        var rangePicker = anychart.ui.rangePicker();
-        // init range picker
-        rangePicker.render(chart);
-
-        // create range selector
-        var rangeSelector = anychart.ui.rangeSelector();
-        // init range selector
-        rangeSelector.render(chart);
-    });
-}
-
-drawChart();
+// var options = {
+// 	series: [{
+// 		name: 'stockOven',
+// 		type: 'candlestick',
+// 		data:[{
+// 			x:
+// 			y:
+// 		}]
+// 	}]
+// }
+	
+	
+// drawChart();
 
 
 
