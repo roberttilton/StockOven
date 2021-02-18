b54af79c2fbd6fe5bdbb90dbbc0b76c90ba602e0
Navid Ebahimi <navidmebrahimi22@gmail.com>
2021-02-18 13:42:47 -0500

    got top 5 movers to appear

--- assets/js/navid.js
+++ assets/js/navid.js
@@ -48,3 +48,26 @@ function fetchStock(stockInput) {
 
 
 fetchStock("GME");
+
+
+fetch("https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/v2/get-movers?region=US&lang=en-US&start=0&count=5", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "49c262adb7msh32c74269c34335fp14ab31jsn4366026eeabe",
+		"x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com"
+	}
+})
+.then(response => response.json()).then(data => {
+	console.log(data);
+	var movers = [];
+	for (let i =0; i<data.finance.result[2].quotes.length; i++){
+		const stockName = data.finance.result[2].quotes[i].symbol;
+		
+		var stockElement = document.createElement("div");
+		stockElement.innerText = stockName ;
+		document.querySelector("#top-movers").appendChild(stockElement);
+	}
+})
+.catch(err => {
+	console.error(err);
+});
