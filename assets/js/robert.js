ccbb17481161da44400bc4364c03974239b62675
Robert Tilton <robtilton2086@gmail.com>
2021-02-18 13:31:55 -0500

    got categories to go into the arrays for future reference with buttons

--- assets/js/robert.js
+++ assets/js/robert.js
@@ -13,13 +13,14 @@ fetch('https://www.reddit.com/r/wallstreetbets/hot.json', {
 			var redditGain = [];
 			var redditLoss = [];
 			var redditNews = [];
+			var redditYolo = [];
 			console.log(redditContent);
 			console.log(data.data.children[0]);
 			for (var i = 0; i < data.data.children.length; i++) {
 				if ((data.data.children[i].data.score) > 500) {
 					var redditRedirect = document.createElement('button');
 					// post titles parsing
-					var redditPosts = document.createElement('h3');
+					var redditPosts = document.createElement('p');
 					redditPosts.textContent = data.data.children[i].data.title;
 					// username parsing
 					var redditUsers = document.createElement('span');
@@ -27,7 +28,7 @@ fetch('https://www.reddit.com/r/wallstreetbets/hot.json', {
 					// upvote/score parsing
 					var redditScore = document.createElement('span');
 					redditScore.textContent = data.data.children[i].data.score;
-					// url parsing, at the moment not utilized
+					// url parsing, to add permalink to the clickable link functionality
 					const link = data.data.children[i].data.permalink;
 					// adding the results to the page
 					redditRedirect.appendChild(redditPosts);
@@ -36,7 +37,18 @@ fetch('https://www.reddit.com/r/wallstreetbets/hot.json', {
 					redditRedirect.addEventListener("click", function() {
 						window.location.href = `https://www.reddit.com${link}`;
 						console.log(link);
-					})
+					}); if (data.data.children[i].data.link_flair_text === "Daily Discussion") {
+						redditDD.push(data.data.children[i].data.title)
+					} else if (data.data.children[i].data.link_flair_text === "Gain") {
+						redditGain.push(data.data.children[i].data.title)
+					} else if (data.data.children[i].data.link_flair_text === "Loss") {
+						redditLoss.push(data.data.children[i].data.title)
+					} else if (data.data.children[i].data.link_flair_text === "News") {
+						redditNews.push(data.data.children[i].data.title)
+					} else if (data.data.children[i].data.link_flair_text === "YOLO") {
+						redditYolo.push(data.data.children[i].data.title)
+					}
+					console.log(redditYolo);
 					redditRedirect.classList.add("posts");
 					redditContent.appendChild(redditRedirect);
 				}
