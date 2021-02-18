3705a2d8b7e07057e628196de2ad0dec73bff892
Krushil <krushilnaik96@gmail.com>
2021-02-18 17:48:14 -0500

    space out Robert's code and use mock fetch response

--- assets/js/robert.js
+++ assets/js/robert.js
@@ -1,39 +1,48 @@
 // declared variable linking to the Reddit div in the HTML
 var redditContent = document.getElementById('reddit');
 // fetching the data from reddit
-fetch('https://www.reddit.com/r/wallstreetbets/hot.json', {})
+fetch('sample_responses/sample_reddit_response.json', {})
 	.then(function (response) {
 		return response.json();
 	})
-	// function that parses the returned array and applies it where necessary
 	.then(
+		// function that parses the returned array and applies it where necessary
 		function (data) {
 			console.log(redditContent);
 			console.log(data.data.children[0]);
 			for (var i = 0; i < data.data.children.length; i++) {
 				if ((data.data.children[i].data.score) > 500) {
 					var redditRedirect = document.createElement('button');
+					redditRedirect.className = "reddit-post";
+
 					// post titles parsing
-					var redditPosts = document.createElement('h3');
+					var redditPosts = document.createElement('p');
 					redditPosts.textContent = data.data.children[i].data.title;
+					redditPosts.className = "post-title";
+
 					// username parsing
 					var redditUsers = document.createElement('span');
 					redditUsers.textContent = data.data.children[i].data.author;
-					redditUsers.className = "username";
+					redditUsers.className = "post-author";
+
 					// upvote/score parsing
 					var redditScore = document.createElement('span');
 					redditScore.textContent = data.data.children[i].data.score;
 					redditScore.className = "post-score";
+
 					// url parsing, at the moment not utilized
 					const link = data.data.children[i].data.permalink;
+
 					// adding the results to the page
+					redditRedirect.appendChild(redditPosts);
 					redditRedirect.appendChild(redditUsers);
 					redditRedirect.appendChild(redditScore);
-					redditRedirect.appendChild(redditPosts);
+
 					redditRedirect.addEventListener("click", function () {
 						window.open(`https://www.reddit.com${link}`, "_blank");
 					})
-					redditRedirect.classList.add("posts");
+
+					// redditRedirect.classList.add("posts");
 					redditContent.appendChild(redditRedirect);
 				}
 			}
