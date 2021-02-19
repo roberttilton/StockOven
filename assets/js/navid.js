0788379e13a532df88156646777c8e6d071fa7a8
Navid Ebahimi <navidmebrahimi22@gmail.com>
2021-02-18 20:07:08 -0500

    added comments to the code

--- assets/js/navid.js
+++ assets/js/navid.js
@@ -1,4 +1,5 @@
 function fetchStock(stockInput) {
+	// fetch the stock chart data from the yahoo finance api
 	fetch("https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-chart?interval=5m&symbol=" + stockInput + "&range=1d&region=US", {
 			"method": "GET",
 			"headers": {
@@ -6,18 +7,20 @@ function fetchStock(stockInput) {
 				"x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com"
 			}
 		})
+		// convert the response into json
 		.then(response => response.json())
 		.then(data => {
 			var candlePoints = [];
-
+				// Create a for-loop to grab the values from the response array
 			for (let i = 0; i < data.chart.result[0].timestamp.length; i += 5) {
+				// Grab the timestamps from the response's array & multiply by 1000 since in unix
 				const timestamp = new Date(data.chart.result[0].timestamp[i] * 1000);
 
 				const close = data.chart.result[0].indicators.quote[0].close[i];
 				const high = data.chart.result[0].indicators.quote[0].high[i];
 				const low = data.chart.result[0].indicators.quote[0].low[i];
 				const open = data.chart.result[0].indicators.quote[0].open[i];
-
+				// set the x and y axis of our graph
 				candlePoints.push({
 					x: timestamp,
 					y: [open, high, low, close]
@@ -25,7 +28,7 @@ function fetchStock(stockInput) {
 			}
 
 			console.log(candlePoints);
-
+			// create an object for the properties of our graph. Based off ApexCharts rubric.
 			var options = {
 				series: [{
 					name: "candle",
@@ -46,10 +49,10 @@ function fetchStock(stockInput) {
 		});
 }
 
-
+// call the function
 fetchStock("GME");
 
-
+// fetch a response for the top movers
 fetch("https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/v2/get-movers?region=US&lang=en-US&start=0&count=5", {
 	"method": "GET",
 	"headers": {
@@ -57,9 +60,11 @@ fetch("https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/v2/get-movers?regi
 		"x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com"
 	}
 })
+	// convert the response into json
 .then(response => response.json()).then(data => {
 	console.log(data);
 	var movers = [];
+	// create a for loop to grab from the top movers array
 	for (let i =0; i<data.finance.result[2].quotes.length; i++){
 		const stockName = data.finance.result[2].quotes[i].symbol;
 		
